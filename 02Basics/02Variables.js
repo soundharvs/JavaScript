3a5a69b1fdb86d8e9d92352bd153683d5c0aa476
Soundhar <42808072+soundharvs@users.noreply.github.com>
2022-05-10 22:34:20 +0530

    Added Variables -2

---
+++ 02Basics/02Variables.js
@@ -0,0 +1,17 @@
+const uid = "abc123";
+
+var fullName = "Soundhar";
+var email = "soundharvs2001@gmail.com";
+var password = "12345678";
+var confirmPassword = "12345678";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+// console.log(uid);
+// console.log("Full Name is:", fullName);
+// console.log(email);
+console.log(`
+    With Unique ID : ${uid}
+    User is: ${fullName}
+    and his email is: ${email}
+    and uses password: ${password}
+`);
