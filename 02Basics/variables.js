e0704539585cef9da5e877ba042b156c80b81e31
Soundhar <42808072+soundharvs@users.noreply.github.com>
2022-05-10 22:10:06 +0530

    Added Modified basics and variables

---
+++ 02Basics/variables.js
@@ -0,0 +1,10 @@
+var fullName = "Soundhar";
+
+var courseName = "JavaScript Bootcamp";
+var isLoggedIn = false;
+var loggedCount = 34;
+// console.log(loggedCount);
+var paymentMode;
+// console.log(paymentMode);
+paymentMode = "Credit Card";
+console.log(paymentMode);
