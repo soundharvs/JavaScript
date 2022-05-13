422c530eaa3dde4f653b2eae60d8f7aaae6ed1d0
Soundhar <42808072+soundharvs@users.noreply.github.com>
2022-05-13 13:15:03 +0530

    Added conditionals and assignments

---
+++ 02Basics/04Conditionals.js
@@ -0,0 +1,17 @@
+var temperature;
+
+// TO DO: Go to google and get the data
+temperature = 39;
+
+//var result = temperature < 20;
+// console.log(result);
+
+if (temperature === 20){
+    console.log("It's very cold outside");
+}
+
+if (temperature < 30){
+    console.log("It's moderate outside");
+} else{
+    console.log("It's really HOT outside");
+}
---
+++ 02Basics/05ConditionalPurchase.js
@@ -0,0 +1,9 @@
+var isLoggedIn = true;
+var isEmailVerified = false;
+var creditCardDetails = true;
+
+if (isLoggedIn && isEmailVerified && creditCardDetails){
+    console.log("Allow the user to make a Purchase");
+} else{
+    console.log("Don't allow to purchase");
+}
